9cdd1e2ee7bc070c371fc4e6ac2518644ca91a08
Szilard <ivacsony.szilard@gmail.com>
2023-04-03 14:56:08 +0300

    List actors,directors,writers

--- client/public/script.js
+++ client/public/script.js
@@ -1,19 +1,62 @@
-import { data } from '/data.js';
+import { data } from "/data.js";
 
-const loadEvent = function() {
-
+const loadEvent = function () {
   const page = window.location.pathname.substring(1);
   // Write your JavaScript code after this line
-  
+
   console.log("data: ", data);
   console.log("page: ", page);
-  
+
   const rootElement = document.getElementById("root");
 
-  
-  
-  // Write your JavaScript code before this line
+  const listProfessionals = (movieDB, role) => {
+    rootElement.insertAdjacentHTML(
+      `beforeend`,
+      `<section id = ${role}s></section>`
+    );
 
-}
+    movieDB.professionals.forEach((element) => {
+      if (element.roles.includes(role)) {
+        document
+          .querySelector(`#${role}s`)
+          .insertAdjacentHTML(
+            `beforeend`,
+            `<h1 class = ${role}>${element.name}</h1>`
+          );
+      }
+
+      movieDB.movies.forEach(movie => {
+        if(movie[`${role}s`].includes(element.id)) {
+          document
+          .querySelector(`#${role}s`)
+          .insertAdjacentHTML(
+            `beforeend`,
+            `<h4 class = movie>${movie.title}</h4>`
+          );
+        }
+      });
+
+    });
+  };
+
+  switch (page) {
+    case `actors`:
+      listProfessionals(data, `actor`);
+      break;
+
+    case `directors`:
+      listProfessionals(data, `director`);
+      break;
+
+    case `writers`:
+      listProfessionals(data, `writer`);
+      break;
+
+    default:
+      break;
+  }
+
+  // Write your JavaScript code before this line
+};
 
 window.addEventListener("load", loadEvent);
